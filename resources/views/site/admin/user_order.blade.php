7c15fa246669eb0a78bac2f6f8e9a02b416532d8
nguyentienvu4897 <nguyentienvu4897@gmail.com>
2024-12-30 08:59:44 +0700

    fix 30/12

--- resources/views/site/admin/user_order.blade.php
+++ resources/views/site/admin/user_order.blade.php
@@ -54,7 +54,7 @@
                     url: '{{ route('orders.searchData') }}',
                     data: function (d, context) {
                         DATATABLE.mergeSearch(d, context);
-                        d.employee_email = "{{ Auth::guard('client')->user()->email }}";
+                        // d.employee_email = "{{ Auth::guard('client')->user()->email }}";
                     },
                 },
                 columns: [
