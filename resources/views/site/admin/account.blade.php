cdb913b213e806ce71a658e9e73fb8c6f0e3fc94
nguyentienvu4897 <nguyentienvu4897@gmail.com>
2025-06-27 05:38:28 +0700

    update 26/6

--- resources/views/site/admin/account.blade.php
+++ resources/views/site/admin/account.blade.php
@@ -47,7 +47,7 @@
         .page_customer_account .block-account .sticky-top{
             position: sticky;
             top: 25px;
-            z-index: 24;
+            z-index: 7;
         }
 
         @media (max-width: 768px) {
