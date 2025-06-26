db013fe544a025d15a153dabe07f16c82d7c3fce
nguyentienvu4897 <nguyentienvu4897@gmail.com>
2025-06-26 10:50:15 +0700

    update 26/6

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -136,7 +136,7 @@
                     orderable: false,
                     className: "text-center",
                     render: function(data) {
-                        return `<img src="${data ? data.path : 'https://via.placeholder.com/150'}" style="max-width: 55px !important">`;
+                        return `<img src="${data ? data.path : 'https://placeholder.co/55x55'}" style="max-width: 55px !important">`;
                     }
                 },
                 {
